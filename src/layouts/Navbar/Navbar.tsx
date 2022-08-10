26ebe31d920ce03e247764c42e385116d029b2a9
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-10 19:16:15 -0400

    Writing html for navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,23 +1,42 @@
 import React from "react";
 import { ReactPropTypes } from "react";
 import { JsxEmit } from "typescript";
-import styled from "./Navbar.module.css";
+import s from "./Navbar.module.css";
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import logoName from "../../assets/logo-name.svg";
 
 const Navbar: React.FC = () => {
   return (
-    <header className={styled['header']}>
-      <div className={styled['header__content']}>
-        <Link to="/" className={styled['header__link']}>
-          <img className={styled['header__logo']} src={logo} alt="reddit logo" />
-          <img
-            className={styled['header__logo-name']}
-            src={logoName}
-            alt="the name reddit"
+    <header className={s["header"]}>
+      <div className={s["header__content"]}>
+        <div className={s["header__left"]}>
+          <Link to="/" className={s["header__link"]}>
+            <img className={s["header__logo"]} src={logo} alt="reddit logo" />
+            <img
+              className={s["header__logo-name"]}
+              src={logoName}
+              alt="the name reddit"
+            />
+          </Link>
+          <input
+            className={s["header__search-form"]}
+            placeholder="Search Reddit"
           />
-        </Link>
+        </div>
+      </div>
+      <div className={s["header__right"]}>
+        <div className={s["header__buttons"]}>
+          <button className={s["header__button header__button_type_log-in"]}>
+            Log In
+          </button>
+          <button className={s["header__button header__button_type_sign-up"]}>
+            Sign Up
+          </button>
+        </div>
+        <div className={s["header__profile"]}>
+          <button className={s["header__dropdown"]}></button>
+        </div>
       </div>
     </header>
   );
