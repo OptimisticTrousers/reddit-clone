273590ef49128d4de9f84e7a699847ca920da893
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-10 14:09:43 -0400

    Configuring a store

---
+++ src/app/store.tsx
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+interface Action {
+  type: string;
+}
+
+interface Reducer {
+  fn: (state: number, action: Action) => number;
+}
+
+const rootReducer: Reducer = (state = 0, action: Action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = configureStore(rootReducer);
