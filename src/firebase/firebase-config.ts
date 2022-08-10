ade41f0ea8b0bddaeabb205bd1971e8cce5f22fa
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-10 19:49:08 -0400

    Adding a firebase config file

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLwXf77NuoR0ESLwAAgez_wS_gM8V4J7M",
+
+  authDomain: "reddit-clone-23782.firebaseapp.com",
+
+  projectId: "reddit-clone-23782",
+
+  storageBucket: "reddit-clone-23782.appspot.com",
+
+  messagingSenderId: "855149209259",
+
+  appId: "1:855149209259:web:cb03f2ae83010e6feab9ee",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/Subreddit/About/About.tsx
@@ -0,0 +1,11 @@
+import s from './About.module.css';
+
+const About: React.FC = () => {
+  return (
+    <div className={s['about']}>
+      
+    </div>
+  )
+}
+
+export default About;
