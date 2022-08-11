4dd9eac8ca572b720729b0c0a8bda6ddf019ad8c
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-11 10:49:25 -0400

    Adding vote icons to subreddit

--- src/pages/Subreddit/Post/Post.tsx
+++ src/pages/Subreddit/Post/Post.tsx
@@ -1,12 +1,13 @@
 import s from "./Post.module.css";
-
+import { BiUpvote } from "react-icons/bi";
+import { BiDownvote } from "react-icons/bi";
 const Post: React.FC = () => {
   return (
     <div className={s["post"]}>
       <div className={s["post__votes"]}></div>
-      <button className={s["post__upvote"]}></button>
-      <p className={s["post__likes"]}></p>
-      <button className={s["post__downvote"]}></button>
+      <BiUpvote />
+      <p className={s["post__likes"]}>1</p>
+      <BiDownvote />
       <div className={s["post__content"]}>
         <span>Image goes here</span>
         <div className={s["post__info"]}>
@@ -14,41 +15,41 @@ const Post: React.FC = () => {
           <span className={s["post__posted-by"]}>Posted by</span>
           <p className={s["post__author"]}>/u/TheWatchingBug</p>
           <span className={s["post__date"]}>4 months ago</span>
-          {/* {conditional rendering for sticky post} */}
-          <div className={s["post__buttons"]}>
-            <span>Expand</span>
-            <div className={s["post__divider"]}></div>
-            <div className={s["post__comments"]}>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>0 Comments</span>
-              </div>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>Award</span>
-              </div>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>Share</span>
-              </div>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>Save</span>
-              </div>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>Hide</span>
-              </div>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>Report</span>
-              </div>
-              <div className={s["post__interaction"]}>
-                <span>Icon</span>
-                <span>Tip</span>
-              </div>
-              {/* {conditional rendering for live chat} */}
+        </div>
+        {/* {conditional rendering for sticky post} */}
+        <div className={s["post__buttons"]}>
+          <span>Expand</span>
+          <div className={s["post__divider"]}></div>
+          <div className={s["post__interactions"]}>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>0 Comments</span>
             </div>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>Award</span>
+            </div>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>Share</span>
+            </div>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>Save</span>
+            </div>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>Hide</span>
+            </div>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>Report</span>
+            </div>
+            <div className={s["post__interaction"]}>
+              <span>Icon</span>
+              <span>Tip</span>
+            </div>
+            {/* {conditional rendering for live chat} */}
           </div>
         </div>
       </div>
