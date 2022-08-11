43ac91dbb30a2650d2464345489a2fce148d6f7e
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-11 07:06:53 -0400

    Finishing About section layout

--- src/pages/Subreddit/index.tsx
+++ src/pages/Subreddit/index.tsx
@@ -1,8 +1,12 @@
 import s from './index.module.css'
+import About from './About/About';
 
 const Subreddit: React.FC = () => {
   return (
     <div className={s['subreddit']}>
+      <aside>
+        <About />
+      </aside>
     </div>
   )
 }
