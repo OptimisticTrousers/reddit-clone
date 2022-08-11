4dd8c40418df3c912d244d671015f72b755c200e
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-11 11:00:47 -0400

    Styling post

--- src/pages/Subreddit/Post/Post.tsx
+++ src/pages/Subreddit/Post/Post.tsx
@@ -1,20 +1,25 @@
 import s from "./Post.module.css";
 import { BiUpvote } from "react-icons/bi";
 import { BiDownvote } from "react-icons/bi";
+import { BiMessage } from "react-icons/bi";
 const Post: React.FC = () => {
   return (
     <div className={s["post"]}>
-      <div className={s["post__votes"]}></div>
-      <BiUpvote />
-      <p className={s["post__likes"]}>1</p>
-      <BiDownvote />
+      <div className={s["post__votes"]}>
+        <BiUpvote />
+        <p className={s["post__likes"]}>1</p>
+        <BiDownvote />
+      </div>
       <div className={s["post__content"]}>
-        <span>Image goes here</span>
+        {/* <span>Image goes here</span> */}
         <div className={s["post__info"]}>
-          <h3 className={s["post__title"]}>r/onepiee Lounge</h3>
           <span className={s["post__posted-by"]}>Posted by</span>
           <p className={s["post__author"]}>/u/TheWatchingBug</p>
           <span className={s["post__date"]}>4 months ago</span>
+        </div>
+        <div className={s["post__container"]}>
+          <h3 className={s["post__title"]}>r/onepiee Lounge</h3>
+          <p className={s["post__description"]}>That's what we do out here</p>
         </div>
         {/* {conditional rendering for sticky post} */}
         <div className={s["post__buttons"]}>
@@ -22,12 +27,8 @@ const Post: React.FC = () => {
           <div className={s["post__divider"]}></div>
           <div className={s["post__interactions"]}>
             <div className={s["post__interaction"]}>
-              <span>Icon</span>
+              <BiMessage />
               <span>0 Comments</span>
-            </div>
-            <div className={s["post__interaction"]}>
-              <span>Icon</span>
-              <span>Award</span>
             </div>
             <div className={s["post__interaction"]}>
               <span>Icon</span>
@@ -44,10 +45,6 @@ const Post: React.FC = () => {
             <div className={s["post__interaction"]}>
               <span>Icon</span>
               <span>Report</span>
-            </div>
-            <div className={s["post__interaction"]}>
-              <span>Icon</span>
-              <span>Tip</span>
             </div>
             {/* {conditional rendering for live chat} */}
           </div>
