4719be015450521e5ecbeb3d7d22d1475876a56c
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-12 17:50:01 -0400

    Setting up auth slice

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,13 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface authState {
+interface AuthState {
   value: boolean;
 }
 
-const initialState = { value: false } as authState;
+const initialState: AuthState = { value: false };
 
 const authSlice = createSlice({
   name: "userAuth",
   initialState,
-  reducers: {},
+  reducers: {
+    authenticate: (state) => {
+      state.value = !state.value;
+    },
+  },
 });
+
+export const { authenticate } = authSlice.actions;
+
+export default authSlice.reducer;
