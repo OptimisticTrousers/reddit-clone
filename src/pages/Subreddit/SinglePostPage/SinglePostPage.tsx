321935dde032b597f49b393195e5f1426e920529
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-13 18:13:46 -0400

    Writing component for SinglePostPage

--- src/pages/Subreddit/SinglePostPage/SinglePostPage.tsx
+++ src/pages/Subreddit/SinglePostPage/SinglePostPage.tsx
@@ -1,10 +1,14 @@
-import s from './SinglePost.module.css'
+import s from "./SinglePost.module.css";
 
-const SinglePostPatch = () => {
-
+const SinglePostPage = () => {
   return (
     <section>
-      <article className={s["post"]}
+      <article className={s["post"]}>
+        <h2>Title</h2>
+        <p className={s["post__content"]}>description of stuff</p>
+      </article>
     </section>
-  )
-}
+  );
+};
+
+export default SinglePostPage;
