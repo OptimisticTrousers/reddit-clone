0190d435a86e31e54fdbc7600b35901d2545229c
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-13 19:13:29 -0400

    Creating a Comment component

---
+++ src/pages/Subreddit/Comment/Comment.tsx
@@ -0,0 +1,12 @@
+import s from './Comment.module.css'
+const Comment = () => {
+  return (
+    <div className={s["comment"]}>
+      <div className={s["comment__author"]}>
+
+      </div>
+    </div>
+  )
+}
+
+export default Comment
