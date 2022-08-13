10ff2c93ddadd5ccb3b7ccb68bccd2e3137aa287
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-13 16:38:51 -0400

    Writing useEffect for Posts.tsx

--- src/pages/Subreddit/Posts/Posts.tsx
+++ src/pages/Subreddit/Posts/Posts.tsx
@@ -1,7 +1,21 @@
+import { collection, doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import Post from "../Post/Post";
 import s from "./Post.module.css";
+import { db } from "../../../firebase";
+import { DocumentSnapshot, DocumentData } from "firebase/firestore";
 
 const Posts: React.FC = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const subredditPostsRef = doc(db, "subreddit", "posts");
+    getDoc(subredditPostsRef).then((subredditPosts: any) =>
+      // console.log(subredditPosts.data().posts)
+      setPosts(subredditPosts.data().posts)
+    );
+  }, []);
+
   return (
     <div className={s["container"]}>
       <Post />
