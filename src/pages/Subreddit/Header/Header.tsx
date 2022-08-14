312c2a50621cd7ac76b34c9ded6f37ba856e7ca8
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-14 18:26:57 -0400

    Styling Header for individual subreddits

--- src/pages/Subreddit/Header/Header.tsx
+++ src/pages/Subreddit/Header/Header.tsx
@@ -4,19 +4,27 @@ import subredditLogo from "../../../assets/subreddit-logo.svg";
 const Header: React.FC = () => {
   return (
     <div className={s["header"]}>
-      <div className={s["header__container"]}>
-        <span>Background Image</span>
-      </div>
+      <div className={s["header__background"]}></div>
       <div className={s["header__container"]}>
         <div className={s["header__content"]}>
-          <img src={subredditLogo} alt="default subreddit logo" />
-          <div className={s["header__title"]}>
-            <h1>onepieee</h1>
-            <h2>r/onepieee</h2>
+          <img
+            className={s["header__subreddit-picture"]}
+            src={subredditLogo}
+            alt="default subreddit logo"
+          />
+          <div className={s["header__title-container"]}>
+            <div className={s["header__title"]}>
+              <h1 className={s["header__subreddit-name"]}>onepieee</h1>
+              <h2 className={s["header__subreddit-link"]}>r/onepieee</h2>
+            </div>
+            <div className={s["header__buttons"]}>
+              <button className={s["header__button"]}>Join</button>
+            </div>
           </div>
-          <button className={s["header__button"]}>Join</button>
         </div>
       </div>
     </div>
   );
 };
+
+export default Header;
--- src/pages/Subreddit/index.tsx
+++ src/pages/Subreddit/index.tsx
@@ -1,16 +1,18 @@
 import s from "./index.module.css";
 import About from "./About/About";
 import Posts from "./Posts/Posts";
+import Header from "./Header/Header";
 
 const Subreddit: React.FC = () => {
   return (
     <div className={s["subreddit"]}>
+      <Header />
       <main className={s["main"]}>
         <Posts />
+        <aside>
+          <About />
+        </aside>
       </main>
-      <aside>
-        <About />
-      </aside>
     </div>
   );
 };
