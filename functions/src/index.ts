2ad42da988b0b9d96f206f1391ca1e5ef186a4fc
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-14 11:18:56 -0400

    Adding type for cloud function User

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { UserRecord } from "firebase-functions/v1/auth";
 
 admin.initializeApp();
 const db = admin.firestore();
 
 export const createUserDocument = functions.auth
   .user()
-  .onCreate(async (user: any) => {
+  .onCreate(async (user: UserRecord) => {
     const newUser = {
       ...user,
     };
