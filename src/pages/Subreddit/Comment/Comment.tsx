e7be0c7134c55222e8655ea789ad4739c403f6f7
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-14 13:02:19 -0400

    Rendering comments in comments collection

--- src/pages/Subreddit/Comment/Comment.tsx
+++ src/pages/Subreddit/Comment/Comment.tsx
@@ -1,12 +1,20 @@
-import s from './Comment.module.css'
-const Comment = () => {
+import React from "react";
+import s from "./Comment.module.css";
+
+interface Comments {
+  content: string;
+  created_at: string;
+  id: string;
+  subreddit_id: string;
+  updated_at: string;
+  user_id: string;
+}
+const Comment: React.FC<Comments> = (comment) => {
   return (
     <div className={s["comment"]}>
-      <div className={s["comment__author"]}>
-        Example comment
-      </div>
+      <div className={s["comment__author"]}>{comment.content}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
--- src/pages/Subreddit/Comments/Comments.tsx
+++ src/pages/Subreddit/Comments/Comments.tsx
@@ -1,15 +1,27 @@
 import s from "./Comments.module.css";
 import Comment from "../Comment/Comment";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase";
 
 const Comments = () => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const commentsRef = collection(db, "comments");
+
+    getDocs(commentsRef).then((comments) => {
+      setComments(comments);
+    });
+  }, []);
+
   return (
     <div className={s["comments"]}>
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
+      {comments?.map(({ doc }) => {
+        <Comment key={doc.id} comment={doc.data()} />;
+      })}
     </div>
-  )
+  );
 };
 
 export default Comments;
