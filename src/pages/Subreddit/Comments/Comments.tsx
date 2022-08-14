3fbbca6b487289765a8a3ee6da3cb5c4b364e4ae
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-14 07:35:58 -0400

    Rendering Comment inside of Comments

--- src/pages/Subreddit/Comments/Comments.tsx
+++ src/pages/Subreddit/Comments/Comments.tsx
@@ -1,9 +1,13 @@
 import s from "./Comments.module.css";
+import Comment from "../Comment/Comment";
 
 const Comments = () => {
   return (
     <div className={s["comments"]}>
-      {}
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
     </div>
   )
 };
