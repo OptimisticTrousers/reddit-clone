8472232ddaa513c26e7316604a40cc9ac76c9854
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-14 08:29:25 -0400

    Writing cloud function to store new users on the database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,17 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const createUserDocument = functions.auth
+  .user()
+  .onCreate(async (user: any) => {
+    const newUser = {
+      ...user,
+    };
+    db.collection("users").doc(user.uid).set(newUser);
+  });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
