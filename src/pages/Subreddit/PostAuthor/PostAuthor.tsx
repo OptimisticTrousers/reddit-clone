205e9bd73058b532a48100c2d3e562f8c8aa27c7
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 09:13:08 -0400

    PostAuthor conversion

--- src/pages/Subreddit/PostAuthor/PostAuthor.tsx
+++ src/pages/Subreddit/PostAuthor/PostAuthor.tsx
@@ -1,13 +1,14 @@
-import s from "./PostAuthor.module.css";
+import CSSModules from "react-css-modules";
+import styles from "./PostAuthor.module.css";
 
 const PostAuthor = () => {
   return (
-    <div className={s["post-excerpt__info"]}>
-      <span className={s["post-excerpt__posted-by"]}>Posted by</span>
-      <p className={s["post-excerpt__author"]}>/u/TheWatchingBug</p>
-      <span className={s["post-excerpt__date"]}>4 months ago</span>
+    <div styleName="post-excerpt__info">
+      <span styleName="post-excerpt__posted-by">Posted by</span>
+      <p styleName="post-excerpt__author">/u/TheWatchingBug</p>
+      <span styleName="post-excerpt__date">4 months ago</span>
     </div>
   );
 };
 
-export default PostAuthor;
+export default CSSModules(PostAuthor, styles, { allowMultiple: true });
