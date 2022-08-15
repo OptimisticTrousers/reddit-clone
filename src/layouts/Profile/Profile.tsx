f4da2110926b52059c2b2bef74a7bedf8564c89b
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 09:06:23 -0400

    Profile conversion

--- src/layouts/Profile/Profile.tsx
+++ src/layouts/Profile/Profile.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaRegUser } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
-import s from "./Profile.module.css";
+import styles from "./Profile.module.css";
 import onlineStatus from "../../assets/online-status.svg";
 import { getUserName } from "../../firebase";
+import CSSModules from "react-css-modules";
 
 interface Props {
   isLoggedIn: boolean;
@@ -11,27 +12,27 @@ interface Props {
 
 const Profile: React.FC<Props> = ({ isLoggedIn }) => {
   return (
-    <div className={s["profile"]}>
-      <button className={s["profile__button"]}>
+    <div styleName="profile">
+      <button styleName="profile__button">
         {isLoggedIn ? (
-          <span className={s["profile__details"]}>
+          <span styleName="profile__details">
             <img
               src="https://styles.redditmedia.com/t5_g2km0/styles/profileIcon_snood9e1c00c-b1c6-46a9-a231-a6fcd78cdd16-headshot.png?width=256&height=256&frame=1&crop=256:256,smart&s=7bd48be150588ab7dc3dc5c73be4d0dcddeeae8d"
               alt="default reddit avatar"
-              className={s["profile__avatar"]}
+              styleName="profile__avatar"
             />
-            <div className={s["profile__content"]}>
-              <p className={s["profile__username"]}>{getUserName()}</p>
-              <p className={s["profile__karma"]}>130 Karma</p>
+            <div styleName="profile__content">
+              <p styleName="profile__username">{getUserName()}</p>
+              <p styleName="profile__karma">130 Karma</p>
             </div>
           </span>
         ) : (
-          <FaRegUser className={s["profile__button-icon"]} />
+          <FaRegUser styleName="profile__button-icon" />
         )}
-        <IoIosArrowDown className={s["profile__button-icon"]} />
+        <IoIosArrowDown styleName="profile__button-icon" />
       </button>
     </div>
   );
 };
 
-export default Profile;
+export default CSSModules(Profile, styles, { allowMultiple: true });
