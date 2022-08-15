a18deb02c4c903c7445d2630d54b51e7047e2693
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 08:50:21 -0400

    In the middle of Navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import s from "./Navbar.module.css";
+import styles from "./Navbar.module.css";
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import logoName from "../../assets/white-logo-name.svg";
@@ -11,34 +11,35 @@ import { RiSearchLine } from "react-icons/ri";
 import { FaRegUser } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 import Profile from "../Profile/Profile";
+import CSSModules from "react-css-modules";
 
 const Navbar: React.FC = () => {
   const isLoggedIn = useAppSelector(selectAuthStatus);
 
   return (
-    <header className={s["header"]}>
-      <div className={s["header__content"]}>
-        <div className={s["header__left"]}>
-          <Link to="/" className={s["header__links"]}>
-            <img className={s["header__logo"]} src={logo} alt="reddit logo" />
+    <header styleName={s["header"]}>
+      <div styleName={s["header__content"]}>
+        <div styleName={s["header__left"]}>
+          <Link to="/" styleName={s["header__links"]}>
+            <img styleName={s["header__logo"]} src={logo} alt="reddit logo" />
             <img
-              className={s["header__logo-name"]}
+              styleName={s["header__logo-name"]}
               src={logoName}
               alt="the name reddit"
             />
           </Link>
         </div>
-        <div className={s["header__container"]}>
-          <div className={s["header__search-icon-container"]}>
+        <div styleName={s["header__container"]}>
+          <div styleName={s["header__search-icon-container"]}>
             <RiSearchLine className={s["header__search-icon"]} />
           </div>
           <input
-            className={s["header__search-input"]}
+            styleName={s["header__search-input"]}
             placeholder="Search Reddit"
           />
         </div>
-        <div className={s["header__right"]}>
-          <div className={s["header__buttons"]}>
+        <div styleName={s["header__right"]}>
+          <div styleName={s["header__buttons"]}>
             {/* {isLoggedIn ? (
               <h2>User is logged in</h2>
             ) : ( */}
@@ -46,7 +47,7 @@ const Navbar: React.FC = () => {
               onClick={() => {
                 signIn();
               }}
-              className={classNames(
+              styleName={classNames(
                 s["header__button"],
                 s["header__button_type_log-in"]
               )}
@@ -58,14 +59,14 @@ const Navbar: React.FC = () => {
               onClick={() => {
                 signOutUser();
               }}
-              className={classNames(
+              styleName={classNames(
                 s["header__button"],
                 s["header__button_type_sign-up"]
               )}
             >
               {isLoggedIn ? "Sign Out" : " Sign Up"}
             </button>
-            <Profile isLoggedIn={isLoggedIn}/>
+            <Profile isLoggedIn={isLoggedIn} />
           </div>
         </div>
       </div>
@@ -73,4 +74,4 @@ const Navbar: React.FC = () => {
   );
 };
 
-export default Navbar;
+export default CSSModules(Navbar, styles, { allowMultiple: true });
