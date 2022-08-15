9a7f8eb84abae57a67c8dfd0d29f239efca267c5
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 09:13:54 -0400

    PostExcerpt conversion

--- src/pages/Subreddit/PostExcerpt/PostExcerpt.tsx
+++ src/pages/Subreddit/PostExcerpt/PostExcerpt.tsx
@@ -1,4 +1,4 @@
-import s from "./PostExcerpt.module.css";
+import styles from "./PostExcerpt.module.css";
 import { BiUpvote } from "react-icons/bi";
 import { BiDownvote } from "react-icons/bi";
 import { BiMessage } from "react-icons/bi";
@@ -6,23 +6,24 @@ import Votes from "../Votes/Votes";
 import PostAuthor from "../PostAuthor/PostAuthor";
 import PostInteractions from "../PostInteractions/PostInteractions";
 import { DocumentData } from "firebase/firestore";
+import CSSModules from "react-css-modules";
 
 const PostExcerpt: React.FC<DocumentData> = ({ data }) => {
   const { voteStatus, title, description } = data;
   return (
-    <div className={s["post-excerpt"]}>
+    <div styleName="post-excerpt">
       <Votes voteStatus={voteStatus} />
-      <div className={s["post-excerpt__content"]}>
+      <div styleName="post-excerpt__content">
         {/* <span>Image goes here</span> */}
         <PostAuthor />
-        <div className={s["post-excerpt__container"]}>
-          <h3 className={s["post-excerpt__title"]}>{title}</h3>
-          <p className={s["post-excerpt__description"]}>{description}</p>
+        <div styleName="post-excerpt__container">
+          <h3 styleName="post-excerpt__title">{title}</h3>
+          <p styleName="post-excerpt__description">{description}</p>
         </div>
         {/* {conditional rendering for sticky post} */}
-        <div className={s["post__buttons"]}>
+        <div styleName="post__buttons">
           <span>Expand</span>
-          <div className={s["post-excerpt__divider"]}></div>
+          <div styleName="post-excerpt__divider"></div>
           <PostInteractions />
         </div>
       </div>
@@ -30,4 +31,4 @@ const PostExcerpt: React.FC<DocumentData> = ({ data }) => {
   );
 };
 
-export default PostExcerpt;
+export default CSSModules(PostExcerpt, styles, { allowMultiple: true });
