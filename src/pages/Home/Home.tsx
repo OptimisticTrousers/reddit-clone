4e7868ac44c345ed522df523f357bbf2c670a071
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 13:29:09 -0400

    Adding more things to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,21 @@
 import CSSModules from "react-css-modules";
 import Filter from "../../components/Filter/Filter";
+import About from "../Subreddit/About/About";
+import Posts from "../Subreddit/Posts/Posts";
 import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div styleName="home">
       <Filter />
+      <div styleName="home__content">
+        <div className="home__posts">
+          <Posts />
+        </div>
+        <aside>
+          <About />
+        </aside>
+      </div>
     </div>
   );
 };
