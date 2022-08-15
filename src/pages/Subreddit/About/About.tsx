d1835b62417866b1df90d3dba1a42e01e88d9b6d
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 09:07:37 -0400

    About conversion

--- src/pages/Subreddit/About/About.tsx
+++ src/pages/Subreddit/About/About.tsx
@@ -1,33 +1,38 @@
-import s from "./About.module.css";
+import CSSModules from "react-css-modules";
+import styles from "./About.module.css";
 
 const About: React.FC = () => {
   return (
-    <div className={s["about"]}>
-      <div className={s["about__top"]}>
-        <h2 className={s["about__title"]}>About Community</h2>
+    <div styleName="about">
+      <div styleName="about__top">
+        <h2 styleName="about__title">About Community</h2>
       </div>
-      <p className={s["about__description"]}>
+      <p styleName="about__description">
         Make r/onepiece into r/onepiee in r/place
       </p>
-      <div className={s["about__members"]}>
-        <div className={s["about__block"]}>
-          <div className={s["about__number"]}>3</div>
-          <div className={s["about__member"]}>Members</div>
+      <div styleName="about__members">
+        <div styleName="about__block">
+          <div styleName="about__number">3</div>
+          <div styleName="about__member">Members</div>
         </div>
-        <div className={s["about__block"]}>
-          <div className={s["about__number"]}>4</div>
-          <div className={s["about__member"]}>Online</div>
+        <div styleName="about__block">
+          <div styleName="about__number">4</div>
+          <div styleName="about__member">Online</div>
         </div>
       </div>
-      <hr className={s["about__thematic-break"]}></hr>
-      <div className={s["about__cakeday"]}>
-        <span className={s["about__icon"]}></span>
-        <p className={s["about__date"]}>Created Apr 4, 2022</p>
+      <hr styleName="about__thematic-break"></hr>
+      <div styleName="about__cakeday">
+        <span styleName="about__icon"></span>
+        <p styleName="about__date">Created Apr 4, 2022</p>
       </div>
-      <button className={s["about__button about__button_type_create"]}>Create Post</button>
-      <button className={s["about__button about__button_type_options"]}>Community Options</button>
+      <button styleName="about__button about__button_type_create">
+        Create Post
+      </button>
+      <button styleName="about__button about__button_type_options">
+        Community Options
+      </button>
     </div>
   );
 };
 
-export default About;
+export default CSSModules(About, styles, {allowMultiple: true});
