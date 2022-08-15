51fd77e44dab79277687ff1343c745533c133e8c
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-15 13:13:45 -0400

    Adding a black background color

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./layouts/Navbar/Navbar";
 import { useAppSelector } from "./hooks/hooks";
 import Modal from "./components/Modal";
+import CSSModules from "react-css-modules";
+import styles from './App.module.css'
 
 const App: React.FC = () => {
   // const isModalActive = useAppSelector((state) => state.modal);
   return (
-    <div >
+    <>
       {/* {isModalActive && <Modal />} */}
       <Navbar />
-      <main >
+      <div styleName="container">
         <Outlet />
-      </main>
-    </div>
+      </div>
+    </>
   );
 };
 
-export default App;
+export default CSSModules(App, styles, { allowMultiple: true });
