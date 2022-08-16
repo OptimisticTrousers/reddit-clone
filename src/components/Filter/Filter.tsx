ab34130e35b8d8fa76e12ab39b82ead53e578624
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 13:30:09 -0400

    Fixing margins

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,15 +1,27 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import CSSModules from "react-css-modules";
 import Card from "../Card/Card";
-import styles from './Filter.module.css'
+import styles from "./Filter.module.css";
+import { AiOutlinePicture } from "react-icons/ai";
+import { BsLink45Deg } from "react-icons/bs";
 
-const Filter:React.FC = () => {
+const Filter: React.FC = () => {
   return (
-    <Card >
-      <h1>Bob jones</h1>
-      <h1>Bob jones</h1>
-      <h1>Bob jones</h1>
+    <Card>
+      <div styleName="filter">
+        <input styleName="filter__input" />
+        <a>
+          <AiOutlinePicture />
+        </a>
+        <a>
+          <BsLink45Deg />
+        </a>
+      </div>
     </Card>
-  )
-}
+  );
+};
 
-export default CSSModules(Filter, styles, {allowMultiple: true, handleNotFoundStyleName: "log"});
+export default CSSModules(Filter, styles, {
+  allowMultiple: true,
+  handleNotFoundStyleName: "log",
+});
