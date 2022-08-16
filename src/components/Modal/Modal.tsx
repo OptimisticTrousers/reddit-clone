c00ee54a76a3ff8055b8228ab2ef9582518ef405
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 08:33:03 -0400

    Changing the 'Modal' component to be a container that renders children

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,12 @@
 import styles from "./Modal.module.css";
 import CSSModules from "react-css-modules";
 
-const Modal: React.FC = () => {
-  return (
-    <div styleName="modal">
-      <div styleName="modal-content"></div>
-    </div>
-  );
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+const Modal: React.FC<Props> = ({ children }) => {
+  return <div styleName="modal">{children}</div>;
 };
 
 export default CSSModules(Modal, styles, {
