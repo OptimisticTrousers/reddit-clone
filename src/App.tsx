e1fcaad0a713375c711f756d14eb10dbde52e82e
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 09:09:02 -0400

    Adding svg icons and adding content to modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./layouts/Navbar/Navbar";
 import { useAppSelector } from "./hooks/hooks";
-import Modal from "./components/Modal/Modal";
 import CSSModules from "react-css-modules";
 import styles from "./App.module.css";
 import { selectCommunityModalState } from "./features/subreddit/subredditSlice";
+import CommunityModal from "./components/CommunityModal/CommunityModal";
 
 const App: React.FC = () => {
   const communityModalState = useAppSelector(selectCommunityModalState);
 
   return (
     <>
-      {/* {communityModalState && <Modal />} */}
+      {communityModalState && <CommunityModal />}
       <Navbar />
       <div styleName="container">
         <Outlet />
--- src/components/CommunityModal/CommunityModal.tsx
+++ src/components/CommunityModal/CommunityModal.tsx
@@ -1,11 +1,65 @@
 import CSSModules from "react-css-modules";
 import Modal from "../Modal/Modal";
 import styles from "./CommunityModal.module.css";
+import informationIcon from "../../assets/information-icon.svg";
+import selectedRadio from "../../assets/selected-radio.svg";
+import personIcon from "../../assets/person-icon.svg";
 
 const CommunityModal: React.FC = () => {
   return (
     <Modal>
-      <h1>stuff</h1>
+      <div styleName="community-modal">
+        <div styleName="community-modal__container">
+          <h1 styleName="community-modal__title">Create a community</h1>
+          {/* <div styleName="community-modal__container"> */}
+          <div styleName="community-modal__instructions">
+            <h3 styleName="community-modal__name">Name</h3>
+            <p styleName="community-modal__instruction">
+              Community names including capitalization cannot be changed
+              <img src={informationIcon} alt="information icon" />
+            </p>
+          </div>
+          <div styleName="community-modal__form">
+            <input
+              maxLength={21}
+              type="text"
+              styleName="community-modal__input"
+              placeholder="r/gaming"
+            />
+            <div styleName="community-modal__feedback">
+              <p styleName="community-modal__characters-remaining">
+                21 Characters remaining
+              </p>
+              <p styleName="community-modal__feedback-message">
+                A community name is required
+              </p>
+            </div>
+          </div>
+          <div styleName="community-modal__radio">
+            <h3 styleName="community-modal__community-type-title">
+              Community type
+            </h3>
+            <div styleName="community-modal__radio-group">
+              <input type="hidden" value="public">
+                <img src={selectedRadio} alt="user selected radio button" />
+                <div styleName="community-modal__text">
+                  <div styleName="community-modal__icon-container">
+                    <img
+                      src={personIcon}
+                      alt="icon of a faceless person at shoulder level"
+                    />
+                  </div>
+                  <div styleName="community-modal__community-type">Public</div>
+                  <div styleName="community-modal__community-type-description">
+                    Anyone can view, post, and comment to this community
+                  </div>
+                </div>
+              </input>
+            </div>
+          </div>
+          {/* </div> */}
+        </div>
+      </div>
     </Modal>
   );
 };
