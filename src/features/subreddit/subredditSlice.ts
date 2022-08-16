1956d6dbcb05af7d319cdd1260743d2f05d6649f
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 08:39:55 -0400

    Creating a new global state for toggling the 'Create Community' modal

--- src/features/subreddit/subredditSlice.ts
+++ src/features/subreddit/subredditSlice.ts
@@ -3,9 +3,13 @@ import { RootState } from "../../redux/store";
 
 interface AuthState {
   subredditId: string;
+  communityModalState: boolean;
 }
 
-const initialState: AuthState = { subredditId: "krnv57fgYupN9Kdvxit3" };
+const initialState: AuthState = {
+  subredditId: "krnv57fgYupN9Kdvxit3",
+  communityModalState: false,
+};
 
 export const selectSubredditId = (state: RootState) =>
   state.subreddit.subredditId;
@@ -17,9 +21,13 @@ const subredditSlice = createSlice({
     getSubredditId: (state, action) => {
       state.subredditId = action.payload;
     },
+    setCommunityModalState: (state, action) => {
+      state.communityModalState = action.payload;
+    },
   },
 });
 
-export const { getSubredditId } = subredditSlice.actions;
+export const { getSubredditId, setCommunityModalState } =
+  subredditSlice.actions;
 
 export default subredditSlice.reducer;
