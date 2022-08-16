08a7d96c55a7e29f703f356b785a8bd7f0b4e1f2
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 14:15:19 -0400

    Adding filter icons to Filter component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,12 +1,43 @@
 import CSSModules from "react-css-modules";
 import Card from "../Card/Card";
 import styles from "./Filter.module.css";
+import { AiOutlineRocket } from "react-icons/ai";
+import { HiOutlineFire } from "react-icons/hi";
+import { MdOutlineNewReleases } from "react-icons/md";
+import { MdOutlineLeaderboard } from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
+import { BsFillInboxesFill } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Filter: React.FC = () => {
   return (
     <Card>
-      <div>
-        <h1></h1>
+      <div styleName="filter">
+        <div styleName="filter__categories">
+          <a styleName="filter__category">
+            <AiOutlineRocket styleName="filter__icon" />
+            <span>Best</span>
+          </a>
+          <a styleName="filter__category">
+            <HiOutlineFire styleName="filter__icon" />
+            <span>Hot</span>
+          </a>
+          <a styleName="filter__category">
+            <MdOutlineNewReleases styleName="filter__icon" />
+            <span>New</span>
+          </a>
+          <a styleName="filter__category">
+            <MdOutlineLeaderboard styleName="filter__icon" />
+            <span>Top</span>
+          </a>
+          <a styleName="filter__category">
+            <BsThreeDots />
+          </a>
+        </div>
+        <div styleName="filter__category">
+          <BsFillInboxesFill styleName="filter__icon" />
+          <IoIosArrowDown styleName="filter__icon" />
+        </div>
       </div>
     </Card>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,7 @@ import { IoIosArrowUp } from "react-icons/io";
 import PremiumCard from "./PremiumCard/PremiumCard";
 import TopCommunitiesCard from "./TopCommunitiesCard/TopCommunitiesCard";
 import PostCreatorCard from "../../components/PostCreatorCard/PostCreatorCard";
+import Filter from "../../components/Filter/Filter";
 
 const Home: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const Home: React.FC = () => {
       <div styleName="home__content">
         <div className="home__posts">
           <PostCreatorCard />
+          <Filter />
           <Posts />
         </div>
         <aside styleName="aside">
