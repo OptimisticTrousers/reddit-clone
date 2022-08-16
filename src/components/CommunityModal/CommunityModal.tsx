334d40f54a25a53844e895b2900726b26c540303
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 11:40:33 -0400

    Styling Modal

--- src/components/CommunityModal/CommunityModal.tsx
+++ src/components/CommunityModal/CommunityModal.tsx
@@ -6,19 +6,31 @@ import selectedRadio from "../../assets/selected-radio.svg";
 import personIcon from "../../assets/person-icon.svg";
 import eyeIcon from "../../assets/eye-icon.svg";
 import lockIcon from "../../assets/lock-icon.svg";
+import exitIcon from "../../assets/exit-icon.svg";
 
 const CommunityModal: React.FC = () => {
   return (
     <Modal>
       <div styleName="community-modal">
         <div styleName="community-modal__container">
-          <h1 styleName="community-modal__title">Create a community</h1>
+          <div styleName="community-modal__header">
+            <h1 styleName="community-modal__title">Create a community</h1>
+            <img
+              styleName="community-modal__icon"
+              src={exitIcon}
+              alt="exit button icon"
+            />
+          </div>
           {/* <div styleName="community-modal__container"> */}
           <div styleName="community-modal__instructions">
             <h3 styleName="community-modal__name">Name</h3>
             <p styleName="community-modal__instruction">
               Community names including capitalization cannot be changed
-              <img src={informationIcon} alt="information icon" />
+              <img
+                styleName="community-modal__icon"
+                src={informationIcon}
+                alt="information icon"
+              />
             </p>
           </div>
           <div styleName="community-modal__form">
@@ -42,21 +54,25 @@ const CommunityModal: React.FC = () => {
               Community type
             </h3>
             <div styleName="community-modal__radio-group">
-              <input type="hidden" value="public">
-                <img src={selectedRadio} alt="user selected radio button" />
-                <div styleName="community-modal__text">
-                  <div styleName="community-modal__icon-container">
-                    <img
-                      src={personIcon}
-                      alt="icon of a faceless person at shoulder level"
-                    />
-                  </div>
-                  <div styleName="community-modal__community-type">Public</div>
-                  <div styleName="community-modal__community-type-description">
-                    Anyone can view, post, and comment to this community
-                  </div>
+              <input type="hidden" value="public" />
+              <img
+                styleName="community-modal__icon"
+                src={selectedRadio}
+                alt="user selected radio button"
+              />
+              <div styleName="community-modal__text">
+                <div styleName="community-modal__icon-container">
+                  <img
+                    styleName="community-modal__icon"
+                    src={personIcon}
+                    alt="icon of a faceless person at shoulder level"
+                  />
                 </div>
-              </input>
+                <div styleName="community-modal__community-type">Public</div>
+                <div styleName="community-modal__community-type-description">
+                  Anyone can view, post, and comment to this community
+                </div>
+              </div>
             </div>
           </div>
           {/* </div> */}
