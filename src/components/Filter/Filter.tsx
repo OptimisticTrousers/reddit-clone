258f6e1ff443a62138bb32c704efe90c41841b6d
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-16 13:50:34 -0400

    Writing filter component and rendering it

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -9,12 +9,19 @@ const Filter: React.FC = () => {
   return (
     <Card>
       <div styleName="filter">
-        <input styleName="filter__input" />
-        <a>
-          <AiOutlinePicture />
+        <div styleName="filter__picture">
+          <img
+            styleName="filter__profile-icon"
+            src="https://styles.redditmedia.com/t5_g2km0/styles/profileIcon_snood9e1c00c-b1c6-46a9-a231-a6fcd78cdd16-headshot.png?width=256&height=256&frame=1&crop=256:256,smart&s=7bd48be150588ab7dc3dc5c73be4d0dcddeeae8d"
+            alt="default reddit profile"
+          />
+        </div>
+        <input styleName="filter__input" placeholder="Create Post" />
+        <a styleName="filter__icon-link">
+          <AiOutlinePicture styleName="filter__icon" />
         </a>
-        <a>
-          <BsLink45Deg />
+        <a styleName="filter__icon-link">
+          <BsLink45Deg styleName="filter__icon" />
         </a>
       </div>
     </Card>
