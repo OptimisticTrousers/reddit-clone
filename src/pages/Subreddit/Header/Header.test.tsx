f00236708a5b0e738490286c0db178d84a4c0bab
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-18 10:37:57 -0400

    Snapshot test for header

---
+++ src/pages/Subreddit/Header/Header.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import each from "jest-each";
+import Header from "./Header";
+
+describe("Header", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<Header />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
