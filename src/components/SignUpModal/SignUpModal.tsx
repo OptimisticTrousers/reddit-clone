ea439ce747401f6803d44ca87a9063fd4042c724
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-18 14:19:26 -0400

    Styling sign up modal

--- src/components/SignUpModal/SignUpModal.tsx
+++ src/components/SignUpModal/SignUpModal.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import CSSModules from "react-css-modules";
+import { Link } from "react-router-dom";
 import Modal from "../Modal/Modal";
 import styles from "./SignUpModal.module.css";
 
 const SignUpModal: React.FC = () => {
   return (
     <Modal>
-      <h1></h1>
+      <div styleName="sign-up-modal">
+        <div styleName="sign-up-modal__container">
+          <div styleName="sign-up-modal__image"></div>
+          <div styleName="sign-up-modal__content">
+            <form styleName="sign-up-modal__form">
+              <h1 styleName="sign-up-modal__title">Sign up</h1>
+              <p styleName="sign-up-modal__description">
+                By continuing, you are setting up a Reddit account and agree to
+                our{" "}
+                <a styleName="sign-up-modal__link" href="https://www.redditinc.com/policies/user-agreement">
+                  User Agreement
+                </a>{" "}
+                and{" "}
+                <a styleName="sign-up-modal__link" href="https://www.reddit.com/policies/privacy-policy">
+                  Privacy Policy
+                </a>
+                .
+              </p>
+              <input styleName="sign-up-modal__input" placeholder="EMAIL" />
+              <input styleName="sign-up-modal__input" placeholder="USERNAME" />
+              <input styleName="sign-up-modal__input" placeholder="PASSWORD" />
+              <input
+                styleName="sign-up-modal__input"
+                placeholder="CONFIRM PASSWORD"
+              />
+              <button styleName="sign-up-modal__button" type="submit">
+                Continue
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
