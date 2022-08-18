c5fc8bf1183698a6b59f9fbf67a60eaeb9bd7f0d
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-18 18:24:40 -0400

    Adding new state to redux store

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -5,14 +5,22 @@ import { RootState } from "../../redux/store";
 interface AuthState {
   isLoggedIn: boolean;
   signUpModalState: boolean;
+  signInModalState: boolean;
 }
 
-const initialState: AuthState = { isLoggedIn: false, signUpModalState: false };
+const initialState: AuthState = {
+  isLoggedIn: false,
+  signUpModalState: false,
+  signInModalState: false,
+};
 
 export const selectAuthStatus = (state: RootState) => state.auth.isLoggedIn;
 
 export const selectSignUpModalState = (state: RootState) =>
   state.auth.signUpModalState;
+
+export const selectSignInModalState = (state: RootState) =>
+  state.auth.signInModalState;
 
 const authSlice = createSlice({
   name: "auth",
@@ -24,9 +32,13 @@ const authSlice = createSlice({
     toggleSignUpModal: (state) => {
       state.signUpModalState = !state.signUpModalState;
     },
+    toggleSignInModal: (state) => {
+      state.signInModalState = !state.signInModalState;
+    },
   },
 });
 
-export const { setLoginStatus, toggleSignUpModal } = authSlice.actions;
+export const { setLoginStatus, toggleSignUpModal, toggleSignInModal } =
+  authSlice.actions;
 
 export default authSlice.reducer;
