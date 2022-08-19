6c12c822979d47134cefd2c4b14ffeee482419f2
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-19 07:30:37 -0400

    Writing tests for Subreddit

---
+++ src/pages/Subreddit/index.test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import each from "jest-each";
+import Subreddit from ".";
+
+describe("Subreddit", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<Subreddit />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
