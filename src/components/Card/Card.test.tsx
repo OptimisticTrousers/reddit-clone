586d467c9cc1a7ddd76f0d80c3b6dcb54113a51d
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-19 07:12:06 -0400

    Writing tests for Card

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,5 +1,17 @@
-
 import { queryByPlaceholderText, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import each from "jest-each";
+import Card from "./Card";
+
+describe("Card", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(
+      <Card>
+        <div>Mock Children</div>
+      </Card>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
