7aecfe25db08412513c49c467f4668e103c65cad
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-19 07:17:09 -0400

    Writing tests for Modal

---
+++ src/components/Modal/Modal.test.tsx
@@ -0,0 +1,17 @@
+import { queryByPlaceholderText, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import each from "jest-each";
+import Modal from "./Modal";
+
+describe("Modal", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(
+      <Modal>
+        <div>Mock Children</div>
+      </Modal>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
