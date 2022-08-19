e882d824d638a66c6f86dc0ef27e1df11da681f5
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-19 07:42:42 -0400

    Writing tests for PostInteractions

---
+++ src/pages/Subreddit/PostInteractions/PostInteractions.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import each from "jest-each";
+import PostInteractions from "./PostInteractions";
+
+describe("PostInteractions", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<PostInteractions commentsQuantity={0}/>);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
