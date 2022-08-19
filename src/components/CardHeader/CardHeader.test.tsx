244d923cdfa7664d7babc9f880343fe0aa7a79c6
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-19 07:13:56 -0400

    Writing tests for CardHeader

---
+++ src/components/CardHeader/CardHeader.test.tsx
@@ -0,0 +1,13 @@
+import { queryByPlaceholderText, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import each from "jest-each";
+import CardHeader from "./CardHeader";
+
+describe("CardHeader", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<CardHeader />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
