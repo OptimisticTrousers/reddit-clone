225ba8b588b1f1e61ee85cec589304837d9dd22f
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-21 08:23:24 -0400

    Using moment.js to grab the time relative to when the post was created. ie. 'a day ago'

--- src/pages/Subreddit/PostAuthor/PostAuthor.tsx
+++ src/pages/Subreddit/PostAuthor/PostAuthor.tsx
@@ -2,13 +2,24 @@ import React from "react";
 import CSSModules from "react-css-modules";
 import { Link } from "react-router-dom";
 import styles from "./PostAuthor.module.css";
+import moment from "moment";
 
 interface Props {
   subredditName: string;
   userName: string;
+  createdAt: CreatedAt;
 }
 
-const PostAuthor: React.FC<Props> = ({ subredditName, userName }) => {
+interface CreatedAt {
+  seconds: number;
+  nanoseconds: number;
+}
+
+const PostAuthor: React.FC<Props> = ({
+  subredditName,
+  userName,
+  createdAt,
+}) => {
   return (
     <div styleName="post-excerpt__info">
       <Link to={`/r/${subredditName}`}>
@@ -16,7 +27,7 @@ const PostAuthor: React.FC<Props> = ({ subredditName, userName }) => {
       </Link>
       <span styleName="post-excerpt__posted-by">Posted by</span>
       <p styleName="post-excerpt__author">/u/{userName}</p>
-      <span styleName="post-excerpt__date">4 months ago</span>
+      <span styleName="post-excerpt__date">{moment(new Date(createdAt.seconds * 1000)).fromNow()}</span>
     </div>
   );
 };
