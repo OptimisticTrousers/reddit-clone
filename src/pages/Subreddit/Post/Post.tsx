f3d7b7417af14344201cbfc852a81b49cbcdc88a
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-23 09:08:14 -0400

    Fixing bug where redirecting the user after they create a post would crash the site

--- src/pages/Subreddit/Post/Post.tsx
+++ src/pages/Subreddit/Post/Post.tsx
@@ -5,11 +5,18 @@ import { BiMessage } from "react-icons/bi";
 import Votes from "../Votes/Votes";
 import PostAuthor from "../PostAuthor/PostAuthor";
 import PostInteractions from "../PostInteractions/PostInteractions";
-import { collection, doc, DocumentData, writeBatch } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  DocumentData,
+  getDoc,
+  writeBatch,
+} from "firebase/firestore";
 import CSSModules from "react-css-modules";
 import { render } from "@testing-library/react";
 import { db, getUserId } from "../../../firebase";
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
+import { useParams } from "react-router-dom";
 
 interface Props {
   data: DocumentData;
@@ -17,28 +24,47 @@ interface Props {
 }
 
 const Post: React.FC<Props> = (props) => {
+  const { postId } = useParams();
+
+  const [postData, setPostData] = useState<DocumentData | undefined>();
+
+  // console.log(postId)
+
+  useEffect(() => {
+    async function fetchPost() {
+      const postDocRef = doc(db, "posts", postId!);
+
+      const postDoc = await getDoc(postDocRef);
+
+      // console.log(postDoc.data())
+
+      setPostData(postDoc.data());
+    }
+
+    props.data ?? fetchPost();
+  }, []);
   return (
     <div
       styleName={props.renderHover ? "post-excerpt-hover" : "post-excerpt"}
       data-testid="post"
     >
       <Votes
-        voteStatus={props.data?.voteStatus}
-        subredditId={props.data?.subredditId}
+        voteStatus={props.data?.voteStatus ?? postData?.voteStatus}
+        subredditId={props.data?.subredditId ?? postData?.subredditId}
       />
       <div styleName="post-excerpt__content">
         <PostAuthor
-          subredditName={props.data?.subredditName}
-          userName={props.data?.userName}
-          createdAt={props.data?.createdAt}
+          subredditName={props.data?.subredditName ?? postData?.subredditName}
+          userName={props.data?.userName ?? postData?.userName}
+          createdAt={props.data?.createdAt ?? postData?.createdAt}
         />
         <div styleName="post-excerpt__container">
-          <h3 styleName="post-excerpt__title">{props.data?.title}</h3>
-          <p styleName="post-excerpt__description">{props.data?.description}</p>
+          <h3 styleName="post-excerpt__title">{props.data?.title ?? postData?.title}</h3>
+          <p styleName="post-excerpt__description">{props.data?.description ?? postData?.description}</p>
         </div>
         <div styleName="post__buttons">
           <div styleName="post-excerpt__divider"></div>
-          <PostInteractions commentsQuantity={props.data?.commentsQuantity} />
+          <PostInteractions commentsQuantity={props.data?.commentsQuantity ?? postData?.commentsQuantity} />
         </div>
       </div>
     </div>
