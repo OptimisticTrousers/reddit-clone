4732c66afea744d7650602fe58da5e060fcda865
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-23 06:19:05 -0400

    Styling text for comment

--- src/pages/Subreddit/Comment/Comment.tsx
+++ src/pages/Subreddit/Comment/Comment.tsx
@@ -1,7 +1,7 @@
 import { DocumentData } from "firebase/firestore";
 import React from "react";
 import CSSModules from "react-css-modules";
-import { getUser, getUserName } from "../../../firebase";
+import { getUserName } from "../../../firebase";
 import CommentInteractions from "../CommentInteractions/CommentInteractions";
 import styles from "./Comment.module.css";
 
@@ -9,16 +9,21 @@ const Comment: React.FC<DocumentData> = ({ comment }) => {
   return (
     <div styleName="comment">
       <div styleName="comment__user">
-        <img
-          styleName="comment__icon"
-          src="https://styles.redditmedia.com/t5_g2km0/styles/profileIcon_snood9e1c00c-b1c6-46a9-a231-a6fcd78cdd16-headshot.png?width=256&height=256&crop=256:256,smart&s=bba24bf813728096bcea480988469df3e9e45c5d"
-          alt="default profile icon"
-        />
+        <div styleName="comment__profile">
+          <img
+            styleName="comment__icon"
+            src="https://styles.redditmedia.com/t5_g2km0/styles/profileIcon_snood9e1c00c-b1c6-46a9-a231-a6fcd78cdd16-headshot.png?width=256&height=256&crop=256:256,smart&s=bba24bf813728096bcea480988469df3e9e45c5d"
+            alt="default profile icon"
+          />
+        </div>
+
         <div styleName="comment__treadline"></div>
       </div>
       <div styleName="comment__content">
         <div styleName="comment__body">
-          <div styleName="comment__author">{comment.userName === getUserName() && getUserName()}</div>
+          <div styleName="comment__author">
+            {comment.userName === getUserName() && `${getUserName()}`}
+          </div>
           <div styleName="comment__message">{comment.content}</div>
           <CommentInteractions />
         </div>
