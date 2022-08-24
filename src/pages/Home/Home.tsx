e3024d815b3b894b50804bdfc3b5d9b14bc0e474
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-24 13:05:04 -0400

    Creating functions to filter by rising, best, and new

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,8 +6,63 @@ import TopCommunitiesCard from "./TopCommunitiesCard/TopCommunitiesCard";
 import PostCreatorCard from "../../components/PostCreatorCard/PostCreatorCard";
 import Filter from "../../components/Filter/Filter";
 import PersonalHomeCard from "./PersonalHomeCard/PersonalHomeCard";
+import { useState } from "react";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Home: React.FC = () => {
+  const [filteredPosts, setFilteredPosts] = useState();
+
+  async function filterNew() {
+    const postsDocsRef = collection(db, "posts");
+
+    const q = query(postsDocsRef, orderBy("createdAt", "desc"));
+
+    getDocs(q)
+      .then((data: DocumentData) => {
+        setFilteredPosts(data.docs);
+      })
+      .catch((error) => alert(`ERROR: ${error}`));
+  }
+
+  async function filterTop() {
+    const postsDocsRef = collection(db, "posts");
+
+    const q = query(postsDocsRef, orderBy("voteStatus", "desc"));
+
+    getDocs(q)
+      .then((data: DocumentData) => {
+        setFilteredPosts(data.docs);
+      })
+      .catch((error) => alert(`ERROR: ${error}`));
+  }
+
+  async function filterRising() {
+    const postsDocsRef = collection(db, "posts");
+
+    const startOfDay = new Date();
+
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const q = query(
+      postsDocsRef,
+      where("createdAt", ">=", startOfDay),
+      orderBy("voteStatus", "desc")
+    );
+
+    getDocs(q)
+      .then((data: DocumentData) => {
+        setFilteredPosts(data.docs);
+      })
+      .catch((error) => alert(`ERROR: ${error}`));
+  }
   return (
     <div styleName="home">
       <div styleName="home__content">
