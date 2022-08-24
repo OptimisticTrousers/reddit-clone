9eca396447123de01b7415b1efe51dc7bd036509
OptimisticTrousers <tony.isern@yahoo.com>
2022-08-23 20:06:29 -0400

    Adding skeleton to About component

---
+++ src/components/Skeletons/Article.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const Article = props => (
+  <ContentLoader viewBox="0 0 400 160" height={160} width={400} {...props}>
+    <rect x="0" y="13" rx="4" ry="4" width="400" height="9" />
+    <rect x="0" y="29" rx="4" ry="4" width="100" height="8" />
+    <rect x="0" y="50" rx="4" ry="4" width="400" height="10" />
+    <rect x="0" y="65" rx="4" ry="4" width="400" height="10" />
+    <rect x="0" y="79" rx="4" ry="4" width="100" height="10" />
+    <rect x="0" y="99" rx="5" ry="5" width="400" height="200" />
+  </ContentLoader>
+)
+
+Article.metadata = {
+  name: 'RoyalBhati',
+  github: 'royalbhati',
+  description: 'Simple Article',
+  filename: 'Article',
+}
+
+export default Article
--- src/pages/Subreddit/About/About.tsx
+++ src/pages/Subreddit/About/About.tsx
@@ -8,47 +8,60 @@ import { useAppSelector } from "../../../hooks/hooks";
 import styles from "./About.module.css";
 import moment from "moment";
 import { TbCake } from "react-icons/tb";
+import Article from "../../../components/Skeletons/Article";
 
 const About: React.FC = () => {
-  const { description, numberOfMembers, createdAt, name } =
-    useAppSelector(selectCommunityData);
+  const communityData = useAppSelector(selectCommunityData);
 
   const data = useAppSelector(selectCommunityData);
 
   return (
     <Card>
       <CardHeader />
-      <p styleName="about__description">{description}</p>
-      <div styleName="about__members">
-        <div styleName="about__block">
-          <div styleName="about__number">
-            {numberOfMembers?.toLocaleString()}
+      {Object.keys(communityData).length !== 0 ? (
+        <>
+          <p styleName="about__description">{communityData.description}</p>
+          <div styleName="about__members">
+            <div styleName="about__block">
+              <div styleName="about__number">
+                {communityData.numberOfMembers?.toLocaleString()}
+              </div>
+              <div styleName="about__member">Members</div>
+            </div>
+            <div styleName="about__block">
+              <div styleName="about__number">4</div>
+              <div styleName="about__member">Online</div>
+            </div>
           </div>
-          <div styleName="about__member">Members</div>
-        </div>
-        <div styleName="about__block">
-          <div styleName="about__number">4</div>
-          <div styleName="about__member">Online</div>
-        </div>
-      </div>
-      <hr styleName="about__thematic-break"></hr>
-      <div styleName="about__cakeday">
-        <span styleName="about__icon"></span>
-        <p styleName="about__date">
-          <TbCake /> Created{" "}
-          {moment(new Date(createdAt * 1000)).format("MMM DD, YYYY")}
-        </p>
-      </div>
-      <Link to={`/r/${name}/submit`}>
-        <button styleName="about__button about__button_type_create">
-          Create Post
-        </button>
-      </Link>
-      <hr styleName="about__thematic-break"></hr>
-      <button styleName="about__button about__button_type_options">
-        Community Options
-        <IoIosArrowDown />
-      </button>
+          <hr styleName="about__thematic-break"></hr>
+          <div styleName="about__cakeday">
+            <span styleName="about__icon"></span>
+            <p styleName="about__date">
+              <TbCake /> Created{" "}
+              {moment(new Date(communityData.createdAt * 1000)).format(
+                "MMM DD, YYYY"
+              )}
+            </p>
+          </div>
+          <Link to={`/r/${communityData.name}/submit`}>
+            <button styleName="about__button about__button_type_create">
+              Create Post
+            </button>
+          </Link>
+          <hr styleName="about__thematic-break"></hr>
+          <button styleName="about__button about__button_type_options">
+            Community Options
+            <IoIosArrowDown />
+          </button>
+        </>
+      ) : (
+        <Article
+          animate={true}
+          backgroundColor={"#333"}
+          foregroundColor={"#999"}
+          speed={1}
+        />
+      )}
     </Card>
   );
 };
